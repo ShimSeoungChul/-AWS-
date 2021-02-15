27874d44cdbcbe8b02087e98890b2a66bb0118b3
simseoungchul <tlatmdcjf94@naver.com>
2021-02-16 07:27:22 +0900

    수정

--- src/main/java/com/retirementcalculator/web/HelloController.java
+++ src/main/java/com/retirementcalculator/web/HelloController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // 1
+@RestController
 public class HelloController {
-    @GetMapping("/hello") //2
+    @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
@@ -16,5 +16,4 @@ public class HelloController {
     public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
         return new HelloResponseDto(name, amount);
     }
-
 }
